d627ee8aedd35bd7613105c6926ad952a4383a8b
benjakuben <ben.jakuben@gmail.com>
2017-03-07 11:23:53 -0500

    End of stage 3 video 3

--- app/src/main/java/com/teamtreehouse/interactivestory/MainActivity.java
+++ app/src/main/java/com/teamtreehouse/interactivestory/MainActivity.java
@@ -1,5 +1,6 @@
 package com.teamtreehouse.interactivestory;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -29,7 +30,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void startStory() {
-        
+        Intent intent = new Intent(this, StoryActivity.class);
+        startActivity(intent);
     }
 }
 
