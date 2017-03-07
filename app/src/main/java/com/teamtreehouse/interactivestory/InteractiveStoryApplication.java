8fe03122cc4d9ae3e2e3cffde366424e4303603e
benjakuben <ben.jakuben@gmail.com>
2017-03-07 11:00:46 -0500

    End of stage 2 video 4

---
+++ app/src/main/java/com/teamtreehouse/interactivestory/InteractiveStoryApplication.java
@@ -0,0 +1,49 @@
+package com.teamtreehouse.interactivestory;
+
+import android.app.Activity;
+import android.app.Application;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+
+public class InteractiveStoryApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        registerActivityLifecycleCallbacks(new ActivityLifecycleCallbacks() {
+            @Override
+            public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
+                activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+            }
+
+            @Override
+            public void onActivityStarted(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityResumed(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityPaused(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityStopped(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivitySaveInstanceState(Activity activity, Bundle outState) {
+
+            }
+
+            @Override
+            public void onActivityDestroyed(Activity activity) {
+
+            }
+        });
+    }
+}
