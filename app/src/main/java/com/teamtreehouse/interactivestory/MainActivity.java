c76f3198388700f0b34801bda644e4496f78a179
benjakuben <ben.jakuben@gmail.com>
2017-03-07 11:14:26 -0500

    End of stage 3 video 1

--- app/src/main/java/com/teamtreehouse/interactivestory/MainActivity.java
+++ app/src/main/java/com/teamtreehouse/interactivestory/MainActivity.java
@@ -1,13 +1,45 @@
 package com.teamtreehouse.interactivestory;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText nameField;
+    private Button startButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nameField = (EditText)findViewById(R.id.nameEditText);
+        startButton = (Button)findViewById(R.id.startButton);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = nameField.getText().toString();
+                Toast.makeText(MainActivity.this, name, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
