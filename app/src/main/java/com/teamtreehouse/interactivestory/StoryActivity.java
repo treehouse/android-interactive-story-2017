d02c7a9dd9731bae36607b960f0a7279777c5916
benjakuben <ben.jakuben@gmail.com>
2017-03-07 11:35:53 -0500

    End of stage 3 video 5

--- app/src/main/java/com/teamtreehouse/interactivestory/StoryActivity.java
+++ app/src/main/java/com/teamtreehouse/interactivestory/StoryActivity.java
@@ -1,13 +1,21 @@
 package com.teamtreehouse.interactivestory;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class StoryActivity extends AppCompatActivity {
+
+    public static final String TAG = StoryActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_story);
+
+        Intent intent = getIntent();
+        String name = intent.getStringExtra("Name");
+        Log.d(TAG, name);
     }
 }
