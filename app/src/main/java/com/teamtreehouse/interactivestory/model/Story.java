40e918a8517669cef3d3f1d3e0f0af621b95df92
benjakuben <ben.jakuben@gmail.com>
2017-03-09 21:59:29 -0500

    End of stage 5 video 4

--- app/src/main/java/com/teamtreehouse/interactivestory/model/Story.java
+++ app/src/main/java/com/teamtreehouse/interactivestory/model/Story.java
@@ -38,6 +38,13 @@ public class Story {
 
         pages[6] = new Page(R.drawable.page6, R.string.page6);
     }
+
+    public Page getPage(int pageNumber) {
+        if (pageNumber >= pages.length) {
+            pageNumber = 0;
+        }
+        return pages[pageNumber];
+    }
 }
 
 
--- app/src/main/java/com/teamtreehouse/interactivestory/ui/StoryActivity.java
+++ app/src/main/java/com/teamtreehouse/interactivestory/ui/StoryActivity.java
@@ -1,20 +1,38 @@
 package com.teamtreehouse.interactivestory.ui;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.teamtreehouse.interactivestory.R;
+import com.teamtreehouse.interactivestory.model.Page;
+import com.teamtreehouse.interactivestory.model.Story;
 
 public class StoryActivity extends AppCompatActivity {
 
     public static final String TAG = StoryActivity.class.getSimpleName();
 
+    private Story story;
+    private ImageView storyImageView;
+    private TextView storyTextView;
+    private Button choice1Button;
+    private Button choice2Button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_story);
+
+        storyImageView = (ImageView)findViewById(R.id.storyImageView);
+        storyTextView = (TextView)findViewById(R.id.storyTextView);
+        choice1Button = (Button)findViewById(R.id.choice1Button);
+        choice2Button = (Button)findViewById(R.id.choice2Button);
 
         Intent intent = getIntent();
         String name = intent.getStringExtra(getString(R.string.key_name));
@@ -22,6 +40,16 @@ public class StoryActivity extends AppCompatActivity {
             name = "Friend";
         }
         Log.d(TAG, name);
+
+        story = new Story();
+        loadPage(0);
+    }
+
+    private void loadPage(int pageNumber) {
+        Page page = story.getPage(pageNumber);
+
+        Drawable image = ContextCompat.getDrawable(this, page.getImageId());
+        storyImageView.setImageDrawable(image);
     }
 }
 
